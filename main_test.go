7a3622de30c41c9663f411965d26b4af09398088
kevin.macbook <vb735499@gmail.com>
2024-05-08 22:43:06 +0800

    Modify unit test.

--- main_test.go
+++ main_test.go
@@ -1,14 +1,87 @@
+// Copyright 2014 Manu Martinez-Almeida. All rights reserved.
+// Use of this source code is governed by a MIT style
+// license that can be found in the LICENSE file.
+
 package main
 
 import (
-	"fmt"
+	"net/http"
+	"net/http/httptest"
 	"testing"
+
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
 )
 
-func TestPass(t *testing.T) {
-	router := createUploadServer()
-	err := router.SEngine.Run(":8080")
-	if err != nil {
-		fmt.Printf("%v\n", err)
+type header struct {
+	Key   string
+	Value string
+}
+
+// PerformRequest for testing gin router.
+func PerformRequest(r http.Handler, method, path string, headers ...header) *httptest.ResponseRecorder {
+	req := httptest.NewRequest(method, path, nil)
+	for _, h := range headers {
+		req.Header.Add(h.Key, h.Value)
 	}
+	w := httptest.NewRecorder()
+	r.ServeHTTP(w, req)
+	return w
+}
+
+func testRouteOK(method string, t *testing.T) {
+	passed := false
+	passedAny := false
+	r := gin.New()
+	r.Any("/api/query", func(c *gin.Context) {
+		passedAny = true
+	})
+	r.Handle(method, "/api", func(c *gin.Context) {
+		passed = true
+	})
+
+	w := PerformRequest(r, method, "/api")
+	assert.True(t, passed)
+	assert.Equal(t, http.StatusOK, w.Code)
+
+	PerformRequest(r, method, "/api/query")
+	assert.True(t, passedAny)
+}
+
+// TestSingleRouteOK tests that POST route is correctly invoked.
+func testRouteNotOK(method string, t *testing.T) {
+	passed := false
+	router := gin.New()
+	router.Handle(method, "/api/query", func(c *gin.Context) {
+		passed = true
+	})
+
+	w := PerformRequest(router, method, "/api")
+
+	assert.False(t, passed)
+	assert.Equal(t, http.StatusNotFound, w.Code)
+}
+
+func TestRouterGroupRouteOK(t *testing.T) {
+	testRouteOK(http.MethodGet, t)
+	testRouteOK(http.MethodPost, t)
+	testRouteOK(http.MethodPut, t)
+	testRouteOK(http.MethodPatch, t)
+	testRouteOK(http.MethodHead, t)
+	testRouteOK(http.MethodOptions, t)
+	testRouteOK(http.MethodDelete, t)
+	testRouteOK(http.MethodConnect, t)
+	testRouteOK(http.MethodTrace, t)
+}
+
+func TestRouteNotOK(t *testing.T) {
+	testRouteNotOK(http.MethodGet, t)
+	testRouteNotOK(http.MethodPost, t)
+	testRouteNotOK(http.MethodPut, t)
+	testRouteNotOK(http.MethodPatch, t)
+	testRouteNotOK(http.MethodHead, t)
+	testRouteNotOK(http.MethodOptions, t)
+	testRouteNotOK(http.MethodDelete, t)
+	testRouteNotOK(http.MethodConnect, t)
+	testRouteNotOK(http.MethodTrace, t)
 }
