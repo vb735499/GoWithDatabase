8173d3d4538f7d78d2780342693d80736cbca656
kevin.macbook <vb735499@gmail.com>
2024-05-10 22:54:17 +0800

    Clean code style.

--- main.go
+++ main.go
@@ -1,21 +1,6 @@
 package main
 
 func main() {
-	// bucketName := "pic-image"
-	// bucketClient := getClient()
-	// _files := bucketClient.QueryAll(bucketName)
-	// log.Println(toJson(_files))
-
-	// current_path, _ := os.Getwd()
-	// prefix_path := "/imgs/"
-
-	// username := "user1"
-	// filename := "lena.png"
-	// filepath := current_path + prefix_path + username + "/" + filename
-
-	// bucketClient.UploadFile(bucketName, username, filepath)
-	// bucketClient.DownloadFile(bucketName, username, filepath)
-	// bucketClient.DeleteObjects(bucketName, username, []string{filename})
 	router := createUploadServer()
 	err := router.SEngine.Run(":8080")
 	ErrorOccurMsg(err)
